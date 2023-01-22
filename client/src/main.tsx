21bbfad6be28a35517898001fe51c7ea3bcdbea1
tonny0831 <tonnyjansen0831@gmail.com>
2023-01-22 19:44:32 +0530

    frontend setup complete

---
+++ client/src/main.tsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Goerli;
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  // <React.StrictMode>
+  <ThirdwebProvider desiredChainId={activeChainId}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+  // </React.StrictMode>
+);
