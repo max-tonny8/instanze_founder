d8c50aa7317ce511078dfaaa6567b595f0578d5e
tonny0831 <tonnyjansen0831@gmail.com>
2023-01-23 23:15:51 +0530

    navbar completed

---
+++ client/src/components/CustomButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+interface CustomButtonProps {
+    btnType: 'button' | 'submit' | 'reset'
+    title: string
+    handleClick: () => void
+    styles: string
+}
+
+const CustomButton = ({ btnType, title, handleClick, styles }: CustomButtonProps) => {
+  return (
+    <button
+      type={btnType}
+      className={`font-epilogue font-medium text-[16px] leading-[26px] text-white h-[40px] px-4 rounded-[10px] ${styles}`}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  )
+}
+
+export default CustomButton
