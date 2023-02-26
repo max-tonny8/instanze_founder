2fe0a116d369e7c6ef22d2fd31b397d95be43d87
tonny0831 <tonnyjansen0831@gmail.com>
2023-02-26 22:23:24 +0530

    minor bug fixed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { StateContextProvider } from "./context";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Goerli;
